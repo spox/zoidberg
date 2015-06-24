29f29d851be1e304617ac559944958166d5b3496
Chris Roberts <code@chrisroberts.org>
2015-06-24 07:28:26 -0700

    Add loading for weak ref and dead exception

--- lib/zoidberg.rb
+++ lib/zoidberg.rb
@@ -1,12 +1,15 @@
 require 'bogo'
+require 'thread'
 require 'zoidberg/version'
 
 # Why not Zoidberg!?
 module Zoidberg
+  autoload :DeadException, 'zoidberg/shell'
   autoload :Future, 'zoidberg/future'
   autoload :Pool, 'zoidberg/pool'
   autoload :Proxy, 'zoidberg/proxy'
   autoload :Shell, 'zoidberg/shell'
   autoload :Signal, 'zoidberg/signal'
   autoload :Supervise, 'zoidberg/supervise'
+  autoload :WeakRef, 'zoidberg/weak_ref'
 end
