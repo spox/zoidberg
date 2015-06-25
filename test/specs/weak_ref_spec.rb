fe49307b8e6bf110151ae576b67ff89353366852
Chris Roberts <code@chrisroberts.org>
2015-06-25 07:19:55 -0700

    Add behavior coverage for weakrefs

---
+++ test/specs/weak_ref_spec.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+
+describe Zoidberg::WeakRef do
+
+  let(:weakref){ Zoidberg::WeakRef }
+
+  it 'should act like the thing it references' do
+    thing = 'ohai'
+    ref = weakref.new(thing)
+    ref.must_equal thing
+    ref.object_id.must_equal thing.object_id
+  end
+
+  it 'should not be the thing it references' do
+    thing = 'ohai'
+    ref = weakref.new(thing)
+    ref.__id__.wont_equal thing.__id__
+  end
+
+  it 'should allow the thing it references to be garbage collected' do
+    thing = 'ohai'
+    ref = weakref.new(thing)
+    thing = nil
+    ObjectSpace.garbage_collect
+    sleep(0.02)
+    ->{ ref.chars }.must_raise Zoidberg::WeakRef::RecycledException
+  end
+
+end
