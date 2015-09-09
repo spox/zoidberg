fb5209009fcfc1b09a99a2226e82ae1bedf6d151
Chris Roberts <code@chrisroberts.org>
2015-09-09 15:48:14 -0700

    Provide spec for simple timer

---
+++ test/specs/timer.rb
@@ -0,0 +1,62 @@
+require 'minitest/autorun'
+
+describe Zoidberg::Timer do
+
+  before do
+    @timer = Zoidberg::Timer.new
+  end
+
+  after do
+    @timer.terminate
+  end
+
+  let(:timer){ @timer }
+
+  it 'should run action after interval' do
+    value = false
+    timer.after(0.01){ value = true }
+    sleep(0.1)
+    value.must_equal true
+  end
+
+  it 'should run action multiple times' do
+    value = 0
+    timer.every(0.1){ value += 1 }
+    sleep(1)
+    value.must_equal 9
+  end
+
+  it 'should allow pausing actions' do
+    value = 0
+    timer.every(0.1){ value += 1 }
+    sleep(0.2)
+    timer.pause
+    value.must_equal 2
+    sleep(0.2)
+    value.must_equal 2
+  end
+
+  it 'should allow resuming actions' do
+    value = 0
+    timer.every(0.1){ value += 1 }
+    sleep(0.1)
+    timer.pause
+    value.must_equal 1
+    sleep(0.2)
+    value.must_equal 1
+    timer.resume
+    sleep(0.2)
+    value.must_equal 4
+  end
+
+  it 'should allow cancelling actions' do
+    value = 0
+    timer.every(0.1){ value += 1 }
+    sleep(0.1)
+    timer.cancel
+    value.must_equal 1
+    sleep(0.2)
+    value.must_equal 1
+  end
+
+end
