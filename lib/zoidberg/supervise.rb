e1c7d3de454e45f07d9ec09bdcd9ed3a88510eba
Chris Roberts <code@chrisroberts.org>
2015-10-23 16:42:55 -0700

    Pull instance methods out and provide directly via shell

--- lib/zoidberg/supervise.rb
+++ lib/zoidberg/supervise.rb
@@ -3,36 +3,6 @@ require 'zoidberg'
 module Zoidberg
   # Add supervision to instance
   module Supervise
-    # Customized exception type to wrap allowed errors
-    class AbortException < StandardError
-      attr_accessor :original_exception
-
-      def to_s
-        if(original_exception)
-          "#{original_exception.class}: #{original_exception}"
-        else
-          super
-        end
-      end
-    end
-
-    module InstanceMethods
-
-      # Customized method for raising exceptions that have been
-      # properly handled (preventing termination)
-      #
-      # @param e [Exception]
-      # @raises [AbortException]
-      def abort(e)
-        unless(e.is_a?(::Exception))
-          e = StandardError.new(e)
-        end
-        new_e = ::Zoidberg::Supervise::AbortException.new
-        new_e.original_exception = e
-        ::Kernel.raise new_e
-      end
-
-    end
 
     # Include supervision into given class when included
     #
@@ -40,11 +10,6 @@ module Zoidberg
     def self.included(klass)
       unless(klass.include?(Zoidberg::Shell))
         klass.class_eval{ include Zoidberg::Shell }
-      end
-      unless(klass.include?(Zoidberg::Supervise::InstanceMethods))
-        klass.class_eval do
-          include InstanceMethods
-        end
       end
     end
 
